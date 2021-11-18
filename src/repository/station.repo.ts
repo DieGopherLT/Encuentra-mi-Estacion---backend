9c26d7fcd38ceeba3390a810874bd30103621848
Diego López Torres <buyeye09@gmail.com>
2021-11-18 14:45:29 -0600

    Added query to database for station connections and it's respective interface

--- src/repository/station.repo.ts
+++ src/repository/station.repo.ts
@@ -1,4 +1,4 @@
-import { getRepository } from 'typeorm';
+import { getRepository, getManager } from 'typeorm';
 import { Request, Response } from 'express';
 
 import { Station } from '../models/Station.model';
@@ -7,4 +7,25 @@ import { StationConnection } from '../models/StationConnection.models';
 export const getStationsNameAndId = async () => {
     const repo = getRepository(Station);
     return await repo.find({ select: ['id', 'name'] });
+}
+
+export const getStationsForGraph = () => {
+    try {
+        return getManager()
+            .createQueryBuilder(Station, 's1')
+            .select('s1.id', 'originId')
+            .addSelect('s1.`name`', 'originName')
+            .addSelect('s1.latitude', 'originLatitude')
+            .addSelect('s1.longitude', 'originLongitude')
+            .addSelect('sc.weight', 'weight')
+            .addSelect('s2.id', 'destinyId')
+	        .addSelect('s2.`name`', 'destinyName')
+	        .addSelect('s2.latitude', 'destinyLatitude')
+	        .addSelect('s2.longitude', 'destinyLongitude')
+            .innerJoin(StationConnection, 'sc', 's1.id = sc.originId')
+            .innerJoin(Station, 's2', 's2.id = sc.destinyId')
+            .getRawMany()
+    } catch(e: any) {
+        return [];
+    }
 }
---
+++ src/types/connection.ts
@@ -0,0 +1,11 @@
+export interface Connection {
+    originId:         number;
+    originLatitude:   string;
+    originLongitude:  string;
+    weight:           number;
+    destinyId:        number;
+    destinyLatitude:  string;
+    destinyLongitude: string;
+    originName:       string;
+    destinyName:      string;
+}
